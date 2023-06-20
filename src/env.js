8d77d61bb40e16ec389a15686afe4f679107f8c0
CrystalBlockDev <crystal.blockdev@gmail.com>
2023-06-20 10:32:57 +0900

    update env

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 // import deployment from './deployment.json';
 
 export const xummConfig = {
-  name: "zoetic",
+  name: "RizeMarket",
   env: "production",
-  AppId: "yours",
+  AppId: "f8f5f425-8148-4f85-a6d7-59a514fbe6d4",
 };
